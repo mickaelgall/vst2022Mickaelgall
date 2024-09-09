2b5ae349f5eb6d3d4dbc4090115449093cb88465
mickaelgall <mickaelgall6@gmail.com>
2024-09-09 15:05:36 +0200

    codetestv1visualstudio

---
+++ MGprojetVS2022V1/Montant.vb
@@ -0,0 +1,23 @@
+﻿Module Montant
+    Sub Main()
+        Dim montant As Double
+        Dim remise As Double
+        Dim resultat As Double
+        Console.WriteLine("Veuillez taper votre montant")
+        montant = Console.ReadLine()
+        If montant > 2000 And montant < 5000 Then
+            remise = 1
+            Console.WriteLine("Remise de 1%")
+        End If
+        If montant > 5000 Then
+            remise = 2
+            Console.WriteLine("Remise de 2%")
+        End If
+        If montant < 2000 Then
+            Console.WriteLine("Pas de remise")
+        End If
+        resultat = montant * (1 - remise / 100)
+        Console.WriteLine("Le montant net est : " + resultat.ToString())
+        Console.WriteLine("Aurevoir")
+    End Sub
+End Module
